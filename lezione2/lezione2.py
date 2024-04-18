24930c5b1694efa7ec3e8ce7570a021268627ce3
andbar27 <andrea.barbato2796@icloud.com>
2024-04-18 12:41:50 +0200

    svolto esercizio 4

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -27,3 +27,12 @@ author: str = "PopX"
 quote: str = "Sono io u femminiell, u femminiell de papà"
 message: str = f"{author} once said, \"{quote}\""
 print(message)
+
+#   2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, 
+#   represent the famous person’s name using a variable called famous_person.
+#   Then compose your message and represent it with a new variable called message.
+#   Print your message. 
+famous_person: str = "PopX"
+quote: str = "Sono io u femminiell, u femminiell de papà"
+message: str = f"{famous_person} once said, \"{quote}\""
+print(message)
