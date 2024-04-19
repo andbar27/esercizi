813d9749c6e37d7e4a1f1afc05ae1d658b5d0080
andbar27 <andrea.barbato2796@icloud.com>
2024-04-19 13:53:48 +0200

    esercizi svolti fino a 3-7

--- lezione2/lezione2.py
+++ lezione2/lezione2.py
@@ -62,18 +62,113 @@ names: list = ["Luca", "Giovanni", "Mariele"]
 #   such as a motorcycle or a car, and make a list that stores several examples.
 #   Use your list to print a series of statements about these items,
 #   such as “I would like to own a Honda motorcycle.”
-vehicle = ["treno", "auto", "scooter"]
 examples = ["Mi piacciono i nuovi hitachi molto moderni come treni", 
             "Adoro le citycar le puoi parcheggiare ovunque", 
             "Che bello evitare il traffico col motorino"]
-[print(f"{vehicle[i]}: {examples[i]}") for i in range(len(vehicle))]
+[print(_) for _ in examples]
 
 #   3-4. Guest List: If you could invite anyone, living or deceased,
 #   to dinner, who would you invite? Make a list that includes at least three 
 #   people you’d like to invite to dinner. Then use your list to print a message
 #   to each person, inviting them to dinner.
 guest = ["Da Vinci", "De Rossi", "Di Ventura"]
-[print(f"Ciao {_}, vie a cena dai") for _ in guest]
+invited = [f"Ciao {_}, vieni a cena dai" for _ in guest]
+print(invited)
+
+"""
+3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+• Start with your program from Exercise 3-4. 
+    Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it 
+    with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.
+"""
+guest = ["Da Vinci", "De Rossi", "Di Ventura"]
+invited = [f"Ciao {_}, vieni a cena dai" for _ in guest]
+print(invited)
+print("Da Vinci")
+invited[0].replace("Da Vinci", "Gilberto")
+[print(_) for _ in invited]
+
+"""
+3-6. More Guests: You just found a bigger dinner table, so now more space is available. 
+Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, 
+    informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.
+"""
+guest = ["Da Vinci", "De Rossi", "Di Ventura"]
+invited = [f"Ciao {_}, vieni a cena dai" for _ in guest]
+print(invited)
+print("Grande tavolo: grande festa")
+new_guest = ["Pino","Gino","Lino"]
+guest += new_guest
+invited.insert(0, f"Ciao {new_guest[0]}, vieni a cena dai")
+invited.insert(2, f"Ciao {new_guest[len(invited)//2]}, vieni a cena dai")
+invited.append(f"Ciao {new_guest[2]}, vieni a cena dai")
+[print(_) for _ in invited]
+
+"""
+3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time 
+for the dinner, and now you have space for only two guests.
+• Start with your program from Exercise 3-6. Add a new line that prints a message saying 
+    that you can invite only two people for dinner.
+• Use pop() to remove guests from your list one at a time until only two names remain in your list. 
+    Each time you pop a name from your list, print a message to that person 
+    letting them know you’re sorry you can’t invite them to dinner.
+• Print a message to each of the two people still on your list, letting them know 
+    they’re still invited.
+• Use del to remove the last two names from your list, so you have an empty list. 
+    Print your list to make sure you actually have an empty list at the end of your program.
+"""
+print("tavolo grande in ritardo, vi caccio tutti tranne 2")
+for i in range(len(invited)-2):
+    print(f"sorry {guest[-1]}, ti devo cacciare")
+    invited.pop()
+    guest.pop()
+for g in guest:
+    print(f"{g}, sei ancora invitato, daje")
+del(guest)
+try:
+    print(guest)
+except EmptyList: # type: ignore
+    print("la lista è vuota")
+
+"""
+3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a list. Make sure the list is not in alphabetical order.
+• Print your list in its original order. Don’t worry about printing the list neatly; 
+    just print it as a raw Python list.
+• Use sorted() to print your list in alphabetical order without modifying the actual list.
+• Show that your list is still in its original order by printing it.
+• Use sorted() to print your list in reverse-alphabetical order without changing the order 
+    of the original list.
+• Show that your list is still in its original order by printing it again.
+• Use reverse()  to change the order of your list. 
+    Print the list to show that its order has changed.
+• Use reverse() to change the order of your list again. 
+    Print the list to show it’s back to its original order.
+• Use sort() to change your list so it’s stored in alphabetical order. 
+    Print the list to show that its order has been changed.
+• Use sort() to change your list so it’s stored in reverse-alphabetical order.
+Print the list to show that its order has changed.
+"""
+places = ["Lucca", "Rieti", "Pisa", "Ravenna", "Milano"]
+
+"""
+3-9. Dinner Guests: Working with one of the programs from Exercises 3, 
+use len() to print a message indicating the number of people you’re inviting to dinner.
+"""
 
 
+"""
+3-10. Every Function: Think of things you could store in a list. 
+For example, you could make a list of mountains, rivers, countries, cities, languages, 
+or anything else you’d like. Write a program that creates a list containing these items 
+and then uses each function introduced in this chapter at least once.
+"""
 
