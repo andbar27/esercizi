117aee8e2922b682e0f8a07185d9779b581f0be6
andbar27 <andrea.barbato2796@icloud.com>
2024-04-22 16:35:08 +0200

    svolto esercizio 2

---
+++ lezione4bis/esercizio2.py
@@ -0,0 +1,24 @@
+#Stampare la lunghezza dell'ultima parola in una frase
+print("hello world    baby".split(" "))
+
+def lenLastWorld(s: str):
+    return len(s.split(" ")[-1])
+
+print(lenLastWorld("hello world    baby"))
+print(lenLastWorld("hello world    "))
+
+def lenLastWorld(s: str):
+    l: list[str] = s.split(" ")
+    n: int = l.count("")
+    for i in range(n):
+        l.remove("")
+    return(len(l[-1]))
+
+print(lenLastWorld("hello world    baby"))
+print(lenLastWorld("hello world         "))
+
+def lenLastWorld(s: str):
+    return len(s.strip().split(" ")[-1])
+
+print(lenLastWorld("hello world    baby"))
+print(lenLastWorld("hello world         "))
