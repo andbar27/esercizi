c9a15c5753baf52c9044607aaf8f7f1830e57441
andbar27 <andrea.barbato2796@icloud.com>
2024-04-22 16:05:07 +0200

    add comment

--- lezione4bis/lezione4bis.py
+++ lezione4bis/lezione4bis.py
@@ -10,6 +10,9 @@ def is_palindromo(x: int) -> bool:
 # print(is_palindromo(121))
 # print(is_palindromo(-121))
 
+#   [0 - 1 - 2 - 3]     len = 4 len//2 = 2  last control i = 1
+#   [0 - 1 - 2 - 3 - 4] len = 5 len//2 = 2  last control i = 1 - 2 non c'è bisogno perché è uguale
+
 def is_palindromo(x: int) -> bool:
     s: str = str(x)
     lenght: int = len(s) 
