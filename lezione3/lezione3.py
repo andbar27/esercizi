812ffccaf21365b7c3df28637f73d12a69b2eb9f
andbar27 <andrea.barbato2796@icloud.com>
2024-04-26 17:47:35 +0200

    svolti tutti gli esercizi di lezione3

--- lezione3/lezione3.py
+++ lezione3/lezione3.py
@@ -40,8 +40,8 @@ for i in range(1,21):
 and then use a for loop to print the numbers. 
 (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
 """
-for i in range(1000000):
-    print(i+1)
+# for i in range(1000000):
+#     print(i+1)
 """
 4-5. Summing a Million: Make a list of the numbers from one to one million, 
 and then use min() and max() to make sure your list actually starts at one and ends at one million. 
@@ -183,16 +183,35 @@ Create a variable called alien_color and assign it a value of 'green', 'yellow',
     If it is, print a message that the player just earned 5 points.
 • Write one version of this program that passes the if test and another that fails. 
     (The version that fails will have no output.)
-
 """
+alien_color = "green"
+if alien_color == "green":
+    print("Earn 5 points!")
+
+alien_color = "yellow"
+if alien_color == "green":
+    print("Earn 5 points!")
+
 """
 5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, 
 and write an if-else chain.
 • If the alien’s color is green, print a statement that the player just earned 5 points 
     for shooting the alien.
 • If the alien’s color isn’t green, print a statement that the player just earned 10 points.
-• Write one version of this program that runs the if block and another that runs the else block.
+• Write one version of this program that runs the if block and another that runs 
+the else block.
 """
+alien_color = "yellow"
+if alien_color == "green":
+    print("Earn 5 points!")
+else:
+    print("Earn 10 points")
+
+alien_color = "green"
+if alien_color == "green":
+    print("Earn 5 points!")
+else:
+    print("Earn 10 points")
 """
 5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
 
@@ -201,45 +220,111 @@ and write an if-else chain.
 • If the alien is red, print a message that the player earned 15 points.
 • Write three versions of this program, making sure each message is printed 
     for the appropriate color alien.
-
 """
+alien_color = "red"
+if alien_color == "green":
+    print("Earn 5 points!")
+elif alien_color == "yellow":
+    print("Earn 10 points")
+elif alien_color == "red":
+    print("Earn 15 points")
+
+alien_color = "yellow"
+if alien_color == "green":
+    print("Earn 5 points!")
+elif alien_color == "yellow":
+    print("Earn 10 points")
+elif alien_color == "red":
+    print("Earn 15 points")
+
+alien_color = "green"
+if alien_color == "green":
+    print("Earn 5 points!")
+elif alien_color == "yellow":
+    print("Earn 10 points")
+elif alien_color == "red":
+    print("Earn 15 points")
 """
 5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. 
 Set a value for the variable age, and then:
 • If the person is less than 2 years old, print a message that the person is a baby.
-• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
-• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 2 years old but less than 4, print a message that the person is 
+    a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is 
+    a kid.
 • If the person is at least 13 years old but less than 20, print a message that the person 
     is a teenager.
 • If the person is at least 20 years old but less than 65, print a message that the person 
     is an adult.
 • If the person is age 65 or older, print a message that the person is an elder.
 """
+age = 54
+if age < 2:
+    print("that person is a baby")
+elif age >= 2 and age < 4:
+    print("that person is a toddler")
+elif age >= 4 and age < 13:
+    print("that person is a kid")
+elif age >= 13 and age < 20:
+    print("that person is a teenager")
+elif age >= 20 and age < 65:
+    print("that person is an adult")
+else:
+    print("that person is an elder")
+            
 """
-5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent 
-if statements that check for certain fruits in your list.
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of 
+independent if statements that check for certain fruits in your list.
 • Make a list of your three favorite fruits and call it favorite_fruits.
 • Write five if statements. Each should check whether a certain kind of fruit is in your list. 
     If the fruit is in your list, the if block should print a statement, 
     such as You really like Apples!
 """
+favorite_fruit = ["mela", "mandarino", "fico"]
+if "mela" in favorite_fruit:
+    print("A te piacciono veramente le mele!")
+if "banana" in favorite_fruit:
+    print("A te piacciono veramente le banane!")
+if "fico" in favorite_fruit:
+    print("A te piacciono veramente i fichi!")
+if "mandarino" in favorite_fruit:
+    print("A te piacciono veramente i mandarini!")
+if "pera" in favorite_fruit:
+    print("A te piacciono veramente le pere!")
 """
 5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'. 
-Imagine you are writing code that will print a greeting to each user after they log in to a website. 
+Imagine you are writing code that will print a greeting to each user after they log in to a 
+website. 
 Loop through the list, and print a greeting to each user.
 • If the username is 'admin', print a special greeting, such as Hello admin, 
     would you like to see a status report?
 • Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.
-
 """
+usernames = ["john", "luca", "luigi", "pino", "admin"]
+for name in usernames:
+    if(name == "admin"):
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {name}, thank you for logging in again.")
+
 """
 5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
 • If the list is empty, print the message We need to find some users!
 • Remove all of the usernames from your list, and make sure the correct message is printed.
 """
+usernames = ["john", "luca", "luigi", "pino", "admin"]
+usernames.clear()
+if len(usernames) == 0:
+    print("We need to find some users!")
+for name in usernames:
+    if(name == "admin"):
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {name}, thank you for logging in again.")
+
 """
-5-10. Checking Usernames: Do the following to create a program that simulates how websites ensure 
-that everyone has a unique username.
+5-10. Checking Usernames: Do the following to create a program that simulates how websites 
+ensure that everyone has a unique username.
 • Make a list of five or more usernames called current_users.
 • Make another list of five usernames called new_users. 
     Make sure one or two of the new usernames are also in the current_users list.
@@ -249,14 +334,32 @@ that everyone has a unique username.
 • Make sure your comparison is case insensitive. If 'John' has been used, 
     'JOHN' should not be accepted. (To do this, you’ll need to make 
     a copy of current_users containing the lowercase versions of all existing users.)
-
 """
+current_user = ["john", "luca", "luigi", "pino", "admin"]
+new_user = ["JOHN", "luke", "gigi", "Pino"]
+lower_cu = [name.lower() for name in current_user]
+for name in new_user:
+    if name.lower() in lower_cu:
+        print(f"{name} not available, enter new username")
+    else:
+        print(f"{name} is available")
+
 """
 5-11. Ordinal Numbers: Ordinal numbers indicate their position in a list, such as 1st or 2nd. 
 Most ordinal numbers end in th, except 1, 2, and 3.
 • Store the numbers 1 through 9 in a list.
 • Loop through the list.
-• Use an if-elif-else chain inside the loop to print the proper ordinal ending for each number. 
-    Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", 
+• Use an if-elif-else chain inside the loop to print the proper ordinal ending for each 
+    number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", 
     and each result should be on a separate line.
 """
+nums = list(range(1,10))
+for num in nums:
+    if num > 3:
+        print(f"{num}th")
+    elif num == 3:
+        print(f"{num}rd")
+    elif num == 2:
+        print(f"{num}nd")
+    else:
+        print(f"{num}st")
