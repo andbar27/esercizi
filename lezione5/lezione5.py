83dbcc0c92bbfddde1ac549cf75fff0996eac86e
andbar27 <andrea.barbato2796@icloud.com>
2024-05-02 12:59:13 +0200

    svolti esercizi lezione5

--- lezione5/lezione5.py
+++ lezione5/lezione5.py
@@ -110,3 +110,127 @@ def merge_dictionaries(dict1: dict, dict2: dict) -> dict:
         else:
             retDict[key2] = value2
     return retDict
+
+"""
+Write a function called create_playlist() that accepts a playlist name and a variable number of song 
+titles. The function should return a dictionary with the playlist name and a set of songs. 
+Call the function with different numbers of songs to demonstrate flexibility.
+
+Example: create_playlist("Road Trip", {"Song 1", "Song 2"})
+
+Write a function called add_like() that accepts a dictionary, the name of a playlist, and a boolean 
+value indicating whether it is liked (True or False). This function should return an updated 
+dictionary.
+
+Example: add_like(dictionary, “Road Trip”, liked=True)
+"""
+def create_playlist(name: str, songs: list[str]) -> dict:
+    playlist = {name: set()}
+    for song in songs:
+        playlist[name].add(song)
+    return playlist
+
+def add_like(likeDict: dict, album: str, liked: bool = True):
+    retDict = dict()
+    retDict[album]= likeDict[album]
+    retDict["liked"] = liked
+    return retDict
+
+playlist = create_playlist("bugiardo", ("a","b","c","d"))
+print(playlist)
+print(add_like(playlist, "bugiardo"))
+
+"""
+Write a function called add_book() that accepts an author's name and a variable number of book titles 
+authored by them. This function should return a dictionary where the author's name is the key and the 
+value is a list of their books. Demonstrate this function by adding books for different authors.
+
+Example: add_book("Mark Twain", ["The Adventures of Tom Sawyer", "Life on the Mississippi"])
+
+Write a function called delete_book() that accepts a dictionary and the name of the author from whom 
+to remove all details. This function should return an updated dictionary.
+
+Example: delete_book(dictionary, “Mark Twain”)
+"""
+def add_book(author: str, books: list[str]) -> dict:
+    retDict = {author: set()}
+    for book in books:
+        retDict[author].add(book)
+    return retDict
+
+def delete_book(dictionary: dict, author: str) -> dict:
+    import copy
+    retDictionary = copy.deepcopy(dictionary)
+    if(author in dictionary):
+        retDictionary.pop(author)
+
+books = add_book("Mark Twain", ["The Adventures of Tom Sawyer", "Life on the Mississippi"])
+print(books)
+print(delete_book(books, "Mark Twain"))
+
+"""
+Write a build_profile() function that accepts the name , surname,  occupation, location, and age  
+of a person. Make occupation, location, and age optional parameters. 
+Use this function to create profiles for different people, demonstrating how it handles these 
+optional parameters.
+
+Example: build_profile("John", "Doe", occupation="Developer", location="USA", age=30)
+"""
+def build_profile(name: str, sourname: str, 
+                  occupation: str = None, location: str = None, age: int = None) -> dict:
+    profile = {"name": name, "sourname": sourname, 
+               "occupation": occupation, "location": location, "age": age}
+    return profile
+
+"""
+Write a function called plan_event() that accepts an event name, a list of participants, and an hour. 
+The function should return a dictionary that includes the event name and a list of the participants. 
+Call this function with varying numbers of participants to plan different events.
+
+Example: plan_event("Code Workshop", ["Alice", "Bob", "Charlie"],”4pm”)
+
+Write a function called modify_event() that accepts a dictionary, an event name, and new details to 
+modify an already planned event.
+
+Example: modify_event(dictionary, "Code Workshop", ["Alice", "Bob", "Charlie"], ”4pm”)
+"""
+def plan_event(name: str, partecipants: list[str], hour: str) -> dict:
+    event = {"name": name,
+             "partecipants": partecipants,
+             "hour": hour}
+    return event
+
+def modify_event(event: dict, eventName: str, newPartecipants: list[str], newHour: str):
+    if eventName == event["name"]:
+        event["partecipants"] = newPartecipants
+        event["hour"] = newHour
+
+event = plan_event("Code Workshop", ["Alice", "Bob", "Charlie"], "4pm")
+print(event)
+modify_event(event, "Code Workshop", ["Alice", "John", "Charlie"], "4pm")
+print(event)
+
+"""
+Write a function called create_shopping_list() that accepts a store name and any number of items as 
+arguments. It should return a dictionary with the store name and a set of items to buy there. 
+Test the function with different stores and item lists.
+
+Example: create_shopping_list("Grocery Store", {"Milk", "Eggs", "Bread"})
+
+Write a function called print_shopping_list() that accepts a dictionary and a store name, then prints 
+each item from that store's shopping list.
+
+Example: print_shopping_list(dictionary, "Grocery Store")
+"""
+def create_shopping_list(storeName: str, shoppingList: set) -> dict:
+    retDict = {storeName: set(shoppingList)}
+    return retDict
+
+def print_shopping_list(shoppingDict: dict, storeName: str):
+    if storeName in shoppingDict:
+        print(f"shopping list of {storeName}:")
+        for item in shoppingDict[storeName]:
+            print("\t",item)
+
+myShopList = create_shopping_list("Grocery Store", {"Milk", "Eggs", "Bread"})
+print_shopping_list(myShopList, "Grocery Store")
