d089f82f2c584f1ea8a3ca1def42553f658cc0e0
andbar27 <andrea.barbato2796@icloud.com>
2024-05-08 20:25:46 +0200

    finito zoopark, test fatti: zero

--- zoopark/zoopark.py
+++ zoopark/zoopark.py
@@ -1,32 +1,40 @@
 class Animal:
-    def __init__(self, name: str, species: str, age: int,
-                 height: float, width: float,
-                 preferred_habitat: str):
+    def __init__(self, name: str = "", species: str = "", age: float = 0.0027,
+                 height: float = 0, width: float = 0,
+                 preferred_habitat: str = ""):
         self.name = name
         self.species = species
         self.age = age
         self.height = height
         self.width = width
         self.preferred_habitat = preferred_habitat
+        if(age == 0): age = 0.0027
         self.health = round(100 * (1 / self.age), 3)
+        self.fence = None
 
     def __str__(self) -> str:
-        pass
-        
+        message = f"name: {self.name} - species: {self.species} - age: {self.age}" \
+        + f"height: {self.height}  width: {self.width} - habitat: {self.preferred_habitat}" \
+        + f" - health: {self.health}"
+        return message
 
 class Fence:
-    def __init__(self, animals: list[Animal], area: float,
-                 temperature: float, habitat: str):
+    def __init__(self, animals: list[Animal] = [], area: float = 0,
+                 temperature: float = 0, habitat: str = ""):
         self.animals = animals
         self.area = area
         self.temperature = temperature
         self.habitat = habitat
 
     def __str__(self) -> str:
-        pass
+        message = f"area: {self.area} - temp: {self.temperature} - habitat: {self.habitat}\n"
+        for animal in self.animals:
+            message += f"\t\t\t{animal}\n"
+        return message
+
 
 class ZooKeeper:
-    def __init__(self, name: str, surname: str, id: str):
+    def __init__(self, name: str = "", surname: str = "", id: str = ""):
         self.name = name
         self.surname = surname
         self.id = id
@@ -37,6 +45,7 @@ class ZooKeeper:
             if animalArea <= fence.area:
                 fence.area -= animalArea
                 fence.animals.append(animal)
+                animal.fence = fence
         
 
     def remove_animal(self, animal: Animal, fence: Fence):
@@ -44,6 +53,7 @@ class ZooKeeper:
             fence.animals.remove(animal)
             animalArea = animal.height * animal.width
             fence.area += animalArea
+            animal.fence = None
 
     def feed(self, animal: Animal):
         tempHeight = animal.height / 100 * 2
@@ -52,8 +62,7 @@ class ZooKeeper:
         animalArea = animal.height * animal.width
         diffAnimalArea = tempAnimalArea - animalArea
 
-        fence = Fence([], 5, 5, "") 
-        # Cerca recinto in cui è animal
+        fence = animal.fence
 
         if diffAnimalArea <= fence.area:
             fence.area -= diffAnimalArea
@@ -68,21 +77,20 @@ class ZooKeeper:
         for animal in animals:
             areaAnimal = animal.height * animal.width
             totalAnimalArea += areaAnimal
-
+        if fence.area == 0:
+            return totalAnimalArea
         areaRatio = totalAnimalArea / fence.area
-        if(areaRatio > 0):
-            return areaRatio
-        return totalAnimalArea
+        return areaRatio
 
     def __str__(self) -> str:
-        pass
+        message = f"name: {self.name} - surname: {self.surname} - id: {self.id}"
 
 
 
 class Zoo:
-    def __init__(self, fences: list[Fence], 
-                 zookeepers: list[ZooKeeper], 
-                 name: str = "Zoo"):
+    def __init__(self, name: str = "Zoo",
+                 fences: list[Fence] = [], 
+                 zookeepers: list[ZooKeeper] = []):
         self.name = name
         self.fences = fences
         self.zookeepers = zookeepers
