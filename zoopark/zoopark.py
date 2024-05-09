7ba17bfe4e40a8abebe9230635c971fccdd50bde
andbar27 <andrea.barbato2796@icloud.com>
2024-05-09 20:26:48 +0200

    fatti un po' di test tutto ok, da fare add tramite init di animals

--- zoopark/zoopark.py
+++ zoopark/zoopark.py
@@ -26,9 +26,7 @@ class Animal:
 class Fence:
     def __init__(self, animals: list[Animal] = [], area: float = 0,
                  temperature: float = 0, habitat: str = "Habitat"):
-        self.animals = animals
-        self.idList = id(self.animals)
-        #self.animals = animals.copy()  #   serve verificare le condizioni
+        self.animals = animals.copy()  #   serve verificare le condizioni
         if area < 0: area = 0
         self.area = area
         self.temperature = temperature
@@ -53,6 +51,8 @@ class ZooKeeper:
             if animalArea <= fence.area:
                 fence.area -= animalArea
                 fence.animals.append(animal)
+                if animal.fence:
+                    self.remove_animal(animal, animal.fence)
                 animal.fence = fence
         
 
@@ -87,7 +87,7 @@ class ZooKeeper:
         for animal in animals:
             areaAnimal = animal.height * animal.width
             totalAnimalArea += areaAnimal
-            print(f"tempArea: {animal} - {animal.height} {animal.width} {totalAnimalArea}")
+            #   print(f"tempArea: {animal} - {animal.height} {animal.width} {totalAnimalArea}")
         if fence.area == 0:
             return totalAnimalArea
         areaRatio = totalAnimalArea / fence.area
@@ -111,7 +111,7 @@ class Zoo:
         print(self)
 
     def __str__(self) -> str:
-        sepFence = "#" * 30
+        sepFence = "#" * 30 + "\n"
         message = self.name
         
         message += "\n\n\tList of Fances:\n"
@@ -124,44 +124,13 @@ class Zoo:
         
         return message
 
-"""
-a1 = Animal()
-a2 = Animal()
-zk1 = ZooKeeper()
-f1 = Fence()
-z1 = Zoo(fences=[f1], zookeepers=[zk1])
-zk1.add_animal(a1, f1)
-zk1.add_animal(a2, f1)
-z1.describe_zoo()
-#Done
-#print(zk1.clean(f1))
-zk1.feed(a1)
-#print(a1)
-#Done
-print("\n\n")
-lion = Animal(name = "Leo", species = "Lion", age = 2, height = 4, width = 3)
-lionFence = Fence(area = 12.0)
-print("created lionFence: \n", lionFence, "\n\n")
-# print("clean1: ",zk1.clean(lionFence))
-print("add lion to lionFence\n")
-zk1.add_animal(lion, lionFence)
-# print("clean2 (after add): ",zk1.clean(lionFence))
-# print(lion)
-# print("feed")
-# zk1.feed(lion)
 
-# print(zk1.clean(lionFence))
-# print(lion)
-# print("clean4: ",zk1.clean(lionFence))
 
-# print("\n\n")
-z1.describe_zoo()
-"""
-a1 = Animal(name="a1")
-a2 = Animal(name="a2")
+a1 = Animal(name="a1", height=2, width=2)
+a2 = Animal(name="a2", height=3, width=1)
 a3 = Animal(name="a3")
 a4 = Animal(name="a4")
-f1 = Fence()
+f1 = Fence(area=6)
 f2 = Fence()
 
 zk1 = ZooKeeper()
@@ -170,60 +139,35 @@ zk1.add_animal(a2, f1)
 zk1.add_animal(a3, f2)
 zk1.add_animal(a4, f2)
 
-flag = f1.idList == f2.idList
-print("id list f1 == f2 ",flag)
-
 f3 = Fence()
-print("id list f1 == f3 ",f3.idList == f1.idList)
-
 f4 = Fence()
-print("id list f4 == f3 ",f3.idList == f4.idList)
 
-#CLASSI E METODI UTILIZZATI
-"""
-class Fence:
-    def __init__(self, animals: list[Animal] = [], area: float = 0,
-                 temperature: float = 0, habitat: str = "Habitat"):
-        self.animals = animals
-        self.idList = id(self.animals)
-        if area < 0: area = 0
-        self.area = area
-        self.temperature = temperature
-        self.habitat = habitat
-"""
-"""
-class ZooKeeper:
-    def __init__(self, name: str = "Name", surname: str = "Surname", id: str = "Id"):
-        self.name = name
-        self.surname = surname
-        self.id = id
-    
-    def add_animal(self, animal: Animal, fence: Fence):
-        animalArea = animal.height * animal.width
-        if animal.preferred_habitat == fence.habitat:
-            if animalArea <= fence.area:
-                fence.area -= animalArea
-                fence.animals.append(animal)
-                animal.fence = fence
-        
-"""
-"""
-class Animal:
-    def __init__(self, name: str = "Animal", species: str = "Species", age: float = 0.0027,
-                 height: float = 0.0, width: float = 0.0,
-                 preferred_habitat: str = "Habitat"):
-        self.name = name
-        self.species = species
-        if(age <= 0): 
-            age = 0.0027 
-        self.age = age
-        if(height < 0): 
-            height = 0
-        self.height = height
-        if(width < 0): 
-            width = 0
-        self.width = width
-        self.preferred_habitat = preferred_habitat
-        self.health = round(100 * (1 / self.age), 3)
-        self.fence = None
-"""
+z1 = Zoo(fences = [f1, f2, f3, f4], zookeepers = [zk1])
+
+z1.describe_zoo()
+
+print("\n\n\n")
+
+zk1.remove_animal(a1, f1)
+zk1.remove_animal(a2, f2)
+
+z1.describe_zoo()
+
+print("\n\n\n")
+
+zk1.add_animal(a1, f1)
+zk1.add_animal(a1, f2)
+#zk1.add_animal(a1, f3)
+
+z1.describe_zoo()
+
+for f in [f1,f2,f3,f4]:
+    print(zk1.clean(f))
+
+for i in range(40):
+    zk1.feed(a1)
+
+z1.describe_zoo()
+for f in [f1,f2,f3,f4]:
+    print(zk1.clean(f))
+print(f1.area)
