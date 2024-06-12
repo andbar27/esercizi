7cf83aa63cb7da56932d2cf7cf535b81f5ff6d73
andbar27 <andrea.barbato2796@icloud.com>
2024-06-12 11:36:10 +0200

    minor

--- prova/prova.py
+++ prova/prova.py
@@ -1,3 +1,38 @@
+class Veicolo:
+    
+    def __init__(self, marca: str, modello: str, anno: int):
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+        
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}")
+        
+
+class Auto(Veicolo):
+    
+    def __init__(self, marca: str, modello: str, anno: int, numero_porte: int):
+        super().__init__(marca, modello, anno)
+        self.numero_porte = numero_porte
+        
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Numero Porte: {self.numero_porte}")
+
+
+class Moto(Veicolo):
+
+    def __init__(self, marca: str, modello: str, anno: int, tipo: str):
+        super().__init__(marca, modello, anno)
+        self.tipo = tipo
+
+    def descrivi_veicolo(self):
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Tipo: {self.tipo}")
+
+
+
+
+        
+
 def visit_tree(tree: dict[int, list[int]], n: int, count: int, liv: dict[int, list[int]]):
     print(n)
     liv[count].append(n)
