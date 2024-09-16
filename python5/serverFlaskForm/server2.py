ed2d8a3d93ca2eaeb28e6b857a25b3d51dbacfe4
El14Sqrt <4r0.squarti@gmail.com>
2024-09-16 13:04:02 +0200

    minor

--- python5/serverFlaskForm/server2.py
+++ python5/serverFlaskForm/server2.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+utenti = [['mario', 'password1'],['luigi', 'password3'],['luca', 'password2']]
 
 api = Flask(__name__)
 
@@ -15,12 +17,17 @@ def regko():
     return render_template('reg_ko.html')
 
 @api.route('/login', methods=['GET'])
-def login(context):
-    if(context[username] == 'ciao'):
-        return render_template('reg_ok.html')
-    else:
-        return render_template('reg_ok.html')
-    #return render_template('login.html')
+def login():
+    if request.method == 'GET':
+        user = request.args.get('username')
+        pw = request.args.get('password')
+
+    for u in utenti:
+        if user == u[0]:
+            if pw == u[1]:
+                return render_template('reg_ok.html')
+            
+    return render_template('reg_ko.html')
 
 
 api.run(host='0.0.0.0', port=8085)
