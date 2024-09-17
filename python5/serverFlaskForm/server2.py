dfafcb4c20dc889079b7b5921c3d7a096bc4a369
El14Sqrt <4r0.squarti@gmail.com>
2024-09-17 12:14:26 +0200

    done server flask form

--- python5/serverFlaskForm/server2.py
+++ python5/serverFlaskForm/server2.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-utenti = [['mario', 'password1'],['luigi', 'password3'],['luca', 'password2']]
+utenti = [['mario', 'password1', 'M', 0],['luigi', 'password3', 'M', 0],['luca', 'password2', 'M', 0], ['lucia', 'password4', 'F', 0]]
 
 api = Flask(__name__)
 
@@ -19,13 +19,44 @@ def regko():
 @api.route('/login', methods=['GET'])
 def login():
     if request.method == 'GET':
-        user = request.args.get('username')
-        pw = request.args.get('password')
+        u_req = []
+        u_req.append(request.args.get('username'))
+        u_req.append(request.args.get('password'))
+        u_req.append(request.args.get('sex'))
+        if u_req[2] == "1":
+            u_req[2] = 'M'
+        elif u_req[2] == '2':
+            u_req[2] = 'F'
+        else:
+            u_req[2] = 'N'
+    print(u_req[:3], " - ", utenti[0][:3])
+    for u in utenti:
+        if u_req[:3] == u[:3]:
+            u[3] = 1
+            return render_template('reg_ok.html')
+            
+    return render_template('reg_ko.html')
+
+@api.route('/signin', methods=['GET'])
+def signin():
+    return render_template('signin.html')
+
+
+@api.route('/login1', methods=['GET'])
+def login1():
+    if request.method == 'GET':
+        u_req = []
+        u_req.append(request.args.get('username'))
+        u_req.append(request.args.get('password'))
 
     for u in utenti:
-        if user == u[0]:
-            if pw == u[1]:
-                return render_template('reg_ok.html')
+        if u_req == u[:2] and u[3] == 1:
+            if u[2] == 'M':
+                temp = ' è un maschio'
+            else:
+                temp = ' è una femmina'
+            render_str = "<html><body>"+ u[0] + temp +"</body></html>"
+            return render_str
             
     return render_template('reg_ko.html')
 
